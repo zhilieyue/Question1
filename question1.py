87a1babbebbe1eb490362eeecaa043fccc137f8e
Shuang Han <shuang.han@digitas.com>
2015-03-16 13:57:02 -0400

    Question1

---
+++ question1.py
@@ -0,0 +1,20 @@
+import sys
+def parsefile(test):
+    try:
+        f = open(test,'r')
+        lines = f.readlines()
+    except IOError as e:
+        print e
+    else:
+        cnt=0
+        sum=0.0
+        for line in lines:           
+            # print line
+            res=line.split()
+            for ele in res:
+                sum = sum+float(ele)
+                cnt=cnt+1
+        print 'The total count is:'+str(cnt)
+        print 'The total value is:'+str(sum)
+if __name__ == '__main__':
+    parsefile('/Users/zhileiyue/Desktop/1.txt')
